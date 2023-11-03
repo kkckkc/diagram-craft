881355bbcdab54472129e7edbd9e9a0e8d57b4a4
Magnus Johansson <magnus.johansson@se.ibm.com>
2023-11-03 08:34:58 +0100

    Extract Canvas component

---
+++ src/Canvas.tsx
@@ -0,0 +1,186 @@
+import { useCallback, useRef } from 'react';
+import { Drag, SelectionState } from './state.ts';
+import { Node, NodeApi } from './Node.tsx';
+import { Edge, EdgeApi } from './Edge.tsx';
+import { Selection, SelectionApi, selectionResize } from './Selection.tsx';
+import { Box, Coord } from './geometry.ts';
+import { LoadedDiagram, NodeDef } from './diagram.ts';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+
+const BACKGROUND = 'background';
+
+type ObjectId = typeof BACKGROUND | string;
+
+type DeferedMouseAction = {
+  id: ObjectId;
+  add?: boolean;
+};
+
+export const Canvas = (props: Props) => {
+  const diagram = props.diagram;
+
+  const svgRef = useRef<SVGSVGElement>(null);
+  const selection = useRef<SelectionState>(SelectionState.EMPTY());
+  const drag = useRef<Drag | undefined>(undefined);
+  const nodeRefs = useRef<Record<string, NodeApi | null>>({});
+  const edgeRefs = useRef<Record<string, EdgeApi | null>>({});
+  const selectionRef = useRef<SelectionApi | null>(null);
+  const deferedMouseAction = useRef<DeferedMouseAction | null>(null);
+
+  const updateCursor = useCallback(
+    (coord: Coord) => {
+      if (Box.contains(selection.current, coord)) {
+        svgRef.current!.style.cursor = 'move';
+      } else {
+        svgRef.current!.style.cursor = 'default';
+      }
+    },
+    [svgRef]
+  );
+
+  const onDragStart = useCallback((coord: Coord, type: Drag['type'], original: Box) => {
+    drag.current = { type, offset: coord, original };
+  }, []);
+
+  const onMouseDown = useCallback(
+    (id: ObjectId, coord: Coord, add: boolean) => {
+      const isClickOnBackground = id === BACKGROUND;
+      try {
+        if (add) {
+          if (!isClickOnBackground) {
+            selection.current = SelectionState.toggle(selection.current, diagram.nodeLookup[id]);
+          }
+        } else {
+          if (Box.contains(selection.current, coord)) {
+            deferedMouseAction.current = { id };
+          } else {
+            if (isClickOnBackground) {
+              selection.current = SelectionState.clear(selection.current);
+            } else {
+              selection.current = SelectionState.set(selection.current, diagram.nodeLookup[id]);
+            }
+          }
+        }
+
+        if (SelectionState.isEmpty(selection.current)) return;
+
+        updateCursor(coord);
+
+        const localCoord = Coord.subtract(coord, selection.current.pos);
+        onDragStart(localCoord, 'move', Box.snapshot(selection.current));
+      } finally {
+        selectionRef.current?.repaint();
+      }
+    },
+    [onDragStart, updateCursor]
+  );
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const onMouseUp = useCallback((_id: ObjectId, _coord: Coord) => {
+    try {
+      drag.current = undefined;
+
+      if (deferedMouseAction.current) {
+        if (deferedMouseAction.current.id === BACKGROUND) {
+          selection.current = SelectionState.clear(selection.current);
+          return;
+        } else {
+          selection.current = SelectionState.set(
+            selection.current,
+            diagram.nodeLookup[deferedMouseAction.current.id]
+          );
+          return;
+        }
+      }
+    } finally {
+      selectionRef.current?.repaint();
+      deferedMouseAction.current = null;
+    }
+  }, []);
+
+  const onMouseMove = useCallback(
+    (coord: Coord) => {
+      if (drag.current === undefined) return;
+
+      try {
+        if (drag.current?.type?.startsWith('resize-') || drag.current?.type === 'rotate')
+          return selectionResize(coord, selection.current, drag.current!);
+
+        if (SelectionState.isEmpty(selection.current)) throw new Error('invalid state');
+
+        deferedMouseAction.current = null;
+
+        const d = Coord.subtract(coord, Coord.add(selection.current.pos, drag.current?.offset));
+
+        for (const node of selection.current.elements) {
+          NodeDef.move(node, d);
+
+          SelectionState.recalculate(selection.current);
+        }
+      } finally {
+        updateCursor(coord);
+
+        for (const node of selection.current.elements) {
+          nodeRefs.current[node.id]?.repaint();
+
+          for (const edge of NodeDef.edges(node)) {
+            edgeRefs.current[edge.id]?.repaint();
+          }
+        }
+
+        selectionRef.current?.repaint();
+      }
+    },
+    [updateCursor]
+  );
+
+  return (
+    <>
+      <DndProvider backend={HTML5Backend}>
+        <div>
+          <svg
+            ref={svgRef}
+            width="640px"
+            height="480px"
+            style={{ border: '1px solid white', background: 'white' }}
+            onMouseDown={e => onMouseDown(BACKGROUND, Coord.fromEvent(e.nativeEvent), e.shiftKey)}
+            onMouseUp={e => onMouseUp(BACKGROUND, Coord.fromEvent(e.nativeEvent))}
+            onMouseMove={e => onMouseMove(Coord.fromEvent(e.nativeEvent))}
+          >
+            {diagram.elements.map(e => {
+              const id = e.id;
+              if (e.type === 'edge') {
+                const edge = diagram.edgeLookup[id]!;
+                return (
+                  <Edge
+                    key={id}
+                    ref={(element: EdgeApi) => (edgeRefs.current[id] = element)}
+                    def={edge}
+                  />
+                );
+              } else {
+                const node = diagram.nodeLookup[id]!;
+                return (
+                  <Node
+                    key={id}
+                    ref={(element: NodeApi) => (nodeRefs.current[id] = element)}
+                    isSelected={!!selection.current?.elements?.includes(node)}
+                    onMouseDown={onMouseDown}
+                    def={node}
+                  />
+                );
+              }
+            })}
+
+            <Selection ref={selectionRef} selection={selection.current} onDragStart={onDragStart} />
+          </svg>
+        </div>
+      </DndProvider>
+    </>
+  );
+};
+
+type Props = {
+  diagram: LoadedDiagram;
+};
