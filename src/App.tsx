3f66196cdda750a1829ed3633e51f9e4693dcc2e
Magnus Johansson <magnus.johansson@se.ibm.com>
2023-11-23 17:09:23 +0100

    Fix: strange layout bug - see https://github.com/radix-ui/primitives/issues/346

--- src/App.tsx
+++ src/App.tsx
@@ -77,117 +77,119 @@ const App = () => {
   const actionMap = makeActionMap(defaultCanvasActions)({ diagram: $d });
   const keyMap = defaultMacKeymap;
   return (
-    <DragDropManager>
-      <div id="app" className={'dark-theme'}>
-        <div id="menu">
-          <div id={'menu__menu-button'}>
-            <div>
-              <TbMenu2 size={'1.5rem'} />
+    <div>
+      <DragDropManager>
+        <div id="app" className={'dark-theme'}>
+          <div id="menu">
+            <div id={'menu__menu-button'}>
+              <div>
+                <TbMenu2 size={'1.5rem'} />
+              </div>
+            </div>
+
+            <div id={'menu__tools'} className={'ToolbarRoot'}>
+              <button className={'ToolbarToggleItem'} data-state={'on'}>
+                <TbClick size={'1.1rem'} />
+              </button>
+              <button className={'ToolbarToggleItem'}>
+                <TbLayoutGridAdd size={'1.1rem'} />
+              </button>
+              <button className={'ToolbarToggleItem'}>
+                <TbLine size={'1.1rem'} />
+              </button>
+              <button className={'ToolbarToggleItem'}>
+                <TbTextSize size={'1.1rem'} />
+              </button>
+              <button className={'ToolbarToggleItem'}>
+                <TbPencil size={'1.1rem'} />
+              </button>
+              <button className={'ToolbarToggleItem'}>
+                <TbPolygon size={'1.1rem'} />
+              </button>
+            </div>
+
+            <div id={'menu__document'}>
+              <DocumentSelector
+                diagrams={diagrams}
+                defaultValue={defaultDiagram}
+                onChange={d => {
+                  setDiagram(d);
+                }}
+              />
+            </div>
+
+            <div id={'menu__extra-tools'} className={'ToolbarRoot'}>
+              <button className={'ToolbarButton'} onClick={() => actionMap['ZOOM_IN']?.execute()}>
+                <TbZoomOut size={'1.1rem'} />
+              </button>
+              <button className={'ToolbarButton'} onClick={() => actionMap['ZOOM_OUT']?.execute()}>
+                <TbZoomIn size={'1.1rem'} />
+              </button>
             </div>
           </div>
 
-          <div id={'menu__tools'} className={'ToolbarRoot'}>
-            <button className={'ToolbarToggleItem'} data-state={'on'}>
-              <TbClick size={'1.1rem'} />
-            </button>
-            <button className={'ToolbarToggleItem'}>
-              <TbLayoutGridAdd size={'1.1rem'} />
-            </button>
-            <button className={'ToolbarToggleItem'}>
-              <TbLine size={'1.1rem'} />
-            </button>
-            <button className={'ToolbarToggleItem'}>
-              <TbTextSize size={'1.1rem'} />
-            </button>
-            <button className={'ToolbarToggleItem'}>
-              <TbPencil size={'1.1rem'} />
-            </button>
-            <button className={'ToolbarToggleItem'}>
-              <TbPolygon size={'1.1rem'} />
-            </button>
-          </div>
+          <div id="main-row">
+            <div id="toolbar">
+              <Toolbar actionMap={actionMap} keyMap={keyMap} diagram={$d} />
+            </div>
 
-          <div id={'menu__document'}>
-            <DocumentSelector
-              diagrams={diagrams}
-              defaultValue={defaultDiagram}
-              onChange={d => {
-                setDiagram(d);
-              }}
-            />
-          </div>
+            <SideBar side={'left'}>
+              <SideBarPage icon={TbCategoryPlus}>
+                <PickerToolWindow diagram={$d} />
+              </SideBarPage>
+              <SideBarPage icon={TbStack2}>
+                <LayerToolWindow diagram={$d} />
+              </SideBarPage>
+              <SideBarPage icon={TbSelectAll}>TbSelectAll</SideBarPage>
+              <SideBarPage icon={TbFiles}>TbFiles</SideBarPage>
+              <SideBarPage icon={TbHistory}>TbHistory</SideBarPage>
+            </SideBar>
 
-          <div id={'menu__extra-tools'} className={'ToolbarRoot'}>
-            <button className={'ToolbarButton'} onClick={() => actionMap['ZOOM_IN']?.execute()}>
-              <TbZoomOut size={'1.1rem'} />
-            </button>
-            <button className={'ToolbarButton'} onClick={() => actionMap['ZOOM_OUT']?.execute()}>
-              <TbZoomIn size={'1.1rem'} />
-            </button>
-          </div>
-        </div>
+            <SideBar side={'right'}>
+              <SideBarPage icon={TbInfoCircle}>
+                <InfoToolWindow diagram={$d} />
+              </SideBarPage>
+              <SideBarPage icon={TbPalette}>
+                <ObjectProperties diagram={$d} />
+              </SideBarPage>
+              <SideBarPage icon={TbDatabaseEdit}>TbDatabaseEdit</SideBarPage>
+            </SideBar>
 
-        <div id="main-row">
-          <div id="toolbar">
-            <Toolbar actionMap={actionMap} keyMap={keyMap} diagram={$d} />
-          </div>
-
-          <SideBar side={'left'}>
-            <SideBarPage icon={TbCategoryPlus}>
-              <PickerToolWindow diagram={$d} />
-            </SideBarPage>
-            <SideBarPage icon={TbStack2}>
-              <LayerToolWindow diagram={$d} />
-            </SideBarPage>
-            <SideBarPage icon={TbSelectAll}>TbSelectAll</SideBarPage>
-            <SideBarPage icon={TbFiles}>TbFiles</SideBarPage>
-            <SideBarPage icon={TbHistory}>TbHistory</SideBarPage>
-          </SideBar>
-
-          <SideBar side={'right'}>
-            <SideBarPage icon={TbInfoCircle}>
-              <InfoToolWindow diagram={$d} />
-            </SideBarPage>
-            <SideBarPage icon={TbPalette}>
-              <ObjectProperties diagram={$d} />
-            </SideBarPage>
-            <SideBarPage icon={TbDatabaseEdit}>TbDatabaseEdit</SideBarPage>
-          </SideBar>
-
-          <div id="middle" className={'light-theme'}>
-            <ContextMenu.Root>
-              <ContextMenu.Trigger asChild={true}>
-                <EditableCanvas
-                  key={$d.id}
-                  diagram={$d}
-                  onContextMenu={e => {
-                    contextMenuTarget.current = e.contextMenuTarget;
-                  }}
-                  actionMap={actionMap}
-                  keyMap={keyMap}
-                  onDrop={canvasDropHandler($d)}
-                  onDragOver={canvasDragOverHandler()}
-                />
-              </ContextMenu.Trigger>
-              <ContextMenu.Portal>
-                <ContextMenu.Content className="ContextMenuContent dark-theme">
-                  <ContextMenuDispatcher
-                    state={contextMenuTarget}
-                    createContextMenu={state => {
-                      if (state.type === 'canvas') {
-                        return <CanvasContextMenu actionMap={actionMap} keyMap={keyMap} />;
-                      } else {
-                        return <SelectionContextMenu actionMap={actionMap} keyMap={keyMap} />;
-                      }
+            <div id="middle" className={'light-theme'}>
+              <ContextMenu.Root>
+                <ContextMenu.Trigger asChild={true}>
+                  <EditableCanvas
+                    key={$d.id}
+                    diagram={$d}
+                    onContextMenu={e => {
+                      contextMenuTarget.current = e.contextMenuTarget;
                     }}
+                    actionMap={actionMap}
+                    keyMap={keyMap}
+                    onDrop={canvasDropHandler($d)}
+                    onDragOver={canvasDragOverHandler()}
                   />
-                </ContextMenu.Content>
-              </ContextMenu.Portal>
-            </ContextMenu.Root>
+                </ContextMenu.Trigger>
+                <ContextMenu.Portal>
+                  <ContextMenu.Content className="ContextMenuContent dark-theme">
+                    <ContextMenuDispatcher
+                      state={contextMenuTarget}
+                      createContextMenu={state => {
+                        if (state.type === 'canvas') {
+                          return <CanvasContextMenu actionMap={actionMap} keyMap={keyMap} />;
+                        } else {
+                          return <SelectionContextMenu actionMap={actionMap} keyMap={keyMap} />;
+                        }
+                      }}
+                    />
+                  </ContextMenu.Content>
+                </ContextMenu.Portal>
+              </ContextMenu.Root>
+            </div>
           </div>
         </div>
-      </div>
-    </DragDropManager>
+      </DragDropManager>
+    </div>
   );
 };
 
