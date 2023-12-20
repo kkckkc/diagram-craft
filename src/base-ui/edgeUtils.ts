b110d4350fae0a06743c155f1b2a7c4161649441
Magnus Johansson <magnus.johansson@se.ibm.com>
2023-12-20 14:26:27 +0100

    Refactor: cleanup of edgeUtils

--- src/base-ui/edgeUtils.ts
+++ src/base-ui/edgeUtils.ts
@@ -2,13 +2,55 @@ import { Path } from '../geometry/path.ts';
 import {
   LengthOffsetOnSegment,
   PointOnPath,
-  TimeOffsetOnSegment,
-  WithSegment
+  TimeOffsetOnSegment
 } from '../geometry/pathPosition.ts';
-import { ConnectedEndpoint, DiagramEdge } from '../model/diagramEdge.ts';
+import { ConnectedEndpoint, DiagramEdge, isConnected } from '../model/diagramEdge.ts';
 import { Point } from '../geometry/point.ts';
-import { invariant } from '../utils/assert.ts';
+import { VerifyNotReached } from '../utils/assert.ts';
 import { ArrowShape } from './arrowShapes.ts';
+import { Diagram } from '../model/diagram.ts';
+
+const adjustForArrow = (
+  pointOnPath: PointOnPath | undefined,
+  arrow: ArrowShape | undefined,
+  path: Path,
+  adjust: -1 | 1
+): TimeOffsetOnSegment | undefined => {
+  if (!pointOnPath) return undefined;
+
+  if (arrow) {
+    const baseTOS = PointOnPath.toTimeOffset(pointOnPath, path);
+    const arrowL1 = TimeOffsetOnSegment.toLengthOffsetOnSegment(baseTOS, path);
+    // TODO: This 1 (adjust) is likely the stroke width of the edge
+    arrowL1.segmentD += (arrow.shortenBy ?? 0) + adjust;
+
+    return LengthOffsetOnSegment.toTimeOffsetOnSegment(arrowL1, path);
+  } else {
+    return PointOnPath.toTimeOffset(pointOnPath, path);
+  }
+};
+
+const intersectWithNode = (
+  endpoint: ConnectedEndpoint,
+  endpointPosition: Point,
+  path: Path,
+  diagram: Diagram
+): PointOnPath => {
+  const anchor = endpoint.node.getAnchor(endpoint.anchor);
+  const nodeDefinition = diagram.nodeDefinitions.get(endpoint.node.nodeType);
+  const endIntersections = path.intersections(nodeDefinition.getBoundingPath(endpoint.node));
+
+  if (anchor.clip) {
+    // TODO: Handle multiple intersections
+    return endIntersections?.[0] ?? { point: endpointPosition };
+  } else {
+    const closeIntersections = endIntersections.filter(
+      i =>
+        Point.distance(endpointPosition, i.point) < 1.5 * (endpoint.node.props.stroke?.width ?? 1)
+    );
+    return closeIntersections?.[0] ?? { point: endpointPosition };
+  }
+};
 
 export const clipPath = (
   path: Path,
@@ -18,92 +60,25 @@ export const clipPath = (
 ) => {
   const diagram = edge.diagram!;
 
-  let start: PointOnPath | undefined;
-  let end: PointOnPath | undefined;
+  const start = isConnected(edge.start)
+    ? intersectWithNode(edge.start, edge.startPosition, path, diagram)
+    : undefined;
+  const startOffset = adjustForArrow(start, startArrow, path, 1);
 
-  const intersections: WithSegment<PointOnPath>[] = [];
-  if (edge.isEndConnected()) {
-    const endEndpoint = edge.end as ConnectedEndpoint;
-    const endNode = endEndpoint.node;
-    const anchor = endNode.getAnchor(endEndpoint.anchor);
+  const end = isConnected(edge.end)
+    ? intersectWithNode(edge.end, edge.endPosition, path, diagram)
+    : undefined;
+  const endOffset = adjustForArrow(end, endArrow, path, -1);
 
-    // TODO: Need to handle this better - maybe look for the closest intersection
-    //       to the point - and if longer than a few pixels skip the intersection
-    const endNodeDefinition = diagram.nodeDefinitions.get(endNode.nodeType);
-    const endIntersections = path.intersections(endNodeDefinition.getBoundingPath(endNode));
-
-    if (anchor.clip) {
-      intersections.push(...endIntersections);
-      // TODO: Handle multiple intersections
-      end = endIntersections?.[0] ?? { point: edge.endPosition };
-    } else {
-      const closeIntersections = endIntersections.filter(
-        i => Point.distance(edge.endPosition, i.point) < 1.5 * (endNode.props.stroke?.width ?? 1)
-      );
-      if (closeIntersections) {
-        intersections.push(...closeIntersections);
-      }
-      end = closeIntersections?.[0] ?? { point: edge.endPosition };
-    }
+  if (!startOffset && !endOffset) {
+    return path;
+  } else if (startOffset && endOffset) {
+    return path.split(startOffset, endOffset)[1];
+  } else if (startOffset) {
+    return path.split(startOffset)[1];
+  } else if (endOffset) {
+    return path.split(endOffset)[0];
+  } else {
+    throw new VerifyNotReached();
   }
-  if (edge.isStartConnected()) {
-    const startEndpoint = edge.start as ConnectedEndpoint;
-    const startNode = startEndpoint.node;
-    const anchor = startNode.getAnchor(startEndpoint.anchor);
-    const startNodeDefinition = diagram.nodeDefinitions.get(startNode.nodeType);
-    const startIntersections = path.intersections(startNodeDefinition.getBoundingPath(startNode));
-
-    if (anchor.clip) {
-      intersections.push(...startIntersections);
-      // TODO: Handle multiple intersections
-      start = startIntersections?.[0] ?? { point: edge.startPosition };
-    } else {
-      const closeIntersections = startIntersections.filter(
-        i =>
-          Point.distance(edge.startPosition, i.point) < 1.5 * (startNode.props.stroke?.width ?? 1)
-      );
-      if (closeIntersections) {
-        intersections.push(...closeIntersections);
-      }
-      start = closeIntersections?.[0] ?? { point: edge.startPosition };
-    }
-  }
-
-  if (start) {
-    if (end) {
-      let startOffset: TimeOffsetOnSegment | undefined;
-      let endOffset: TimeOffsetOnSegment | undefined;
-
-      if (startArrow) {
-        const baseTOS = PointOnPath.toTimeOffset(start, path);
-        const arrowL1 = TimeOffsetOnSegment.toLengthOffsetOnSegment(baseTOS, path);
-        // TODO: This 1 is likely the stroke width of the edge
-        arrowL1.segmentD += (startArrow.shortenBy ?? 0) + 1;
-
-        startOffset = LengthOffsetOnSegment.toTimeOffsetOnSegment(arrowL1, path);
-      } else {
-        startOffset = PointOnPath.toTimeOffset(start, path);
-      }
-
-      if (endArrow) {
-        const baseTOS = PointOnPath.toTimeOffset(end, path);
-        const arrowL2 = TimeOffsetOnSegment.toLengthOffsetOnSegment(baseTOS, path);
-        // TODO: This 1 is likely the stroke width of the edge
-        arrowL2.segmentD -= (endArrow.shortenBy ?? 0) + 1;
-
-        endOffset = LengthOffsetOnSegment.toTimeOffsetOnSegment(arrowL2, path);
-      } else {
-        endOffset = PointOnPath.toTimeOffset(end, path);
-      }
-
-      invariant.is.present(startOffset);
-      invariant.is.present(endOffset);
-
-      path = path.split(startOffset, endOffset)[1];
-    } else {
-      path = path.split(PointOnPath.toTimeOffset(start, path))[1];
-    }
-  }
-
-  return path;
 };
