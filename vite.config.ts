8ead5886d0947f3f12607e71b67c446750c4e99a
Magnus Johansson <magnus.johansson@se.ibm.com>
2023-12-27 10:48:42 +0100

    Fix: tests failing due to queueMicrotask not being mocked

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    fakeTimers: {
+      toFake: ['setTimeout', 'clearTimeout', 'setInterval', 'clearInterval', 'queueMicrotask']
+    }
+  },
   build: {
     rollupOptions: {
       output: {
