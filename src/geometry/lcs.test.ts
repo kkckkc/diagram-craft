1f5c901656823582bbee3487f4464cee492f907f
Magnus Johansson <magnus.johansson@se.ibm.com>
2024-01-04 14:26:07 +0100

    Geometry: improve LCS

---
+++ src/geometry/lcs.test.ts
@@ -0,0 +1,42 @@
+import { describe, expect, test } from 'vitest';
+import { LocalCoordinateSystem } from './lcs.ts';
+
+describe('LocalCoordinateSystem', () => {
+  describe('only rotation', () => {
+    test('toLocal ', () => {
+      // 1. Box on origin
+      const lcs = new LocalCoordinateSystem({ x: 0, y: 0, w: 20, h: 30, r: Math.PI / 6 });
+      expect(lcs.toLocal({ x: 0, y: 0 }).x).toBeCloseTo(0);
+      expect(lcs.toLocal({ x: 0, y: 0 }).y).toBeCloseTo(0);
+      expect(lcs.toLocal({ x: 2.32, y: 35.98 }).x).toBeCloseTo(20);
+      expect(lcs.toLocal({ x: 2.32, y: 35.98 }).y).toBeCloseTo(30);
+    });
+
+    test('toGlobal ', () => {
+      // 1. Box on origin
+      const lcs = new LocalCoordinateSystem({ x: 0, y: 0, w: 20, h: 30, r: Math.PI / 6 });
+      expect(lcs.toGlobal({ x: 0, y: 0 }).x).toBeCloseTo(0);
+      expect(lcs.toGlobal({ x: 0, y: 0 }).y).toBeCloseTo(0);
+      expect(lcs.toGlobal({ x: 20, y: 30 }).x).toBeCloseTo(2.32);
+      expect(lcs.toGlobal({ x: 20, y: 30 }).y).toBeCloseTo(35.98);
+    });
+  });
+
+  describe('only translations', () => {
+    test('toLocal', () => {
+      const lcs = new LocalCoordinateSystem({ x: 10, y: 15, w: 20, h: 30, r: 0 });
+      expect(lcs.toLocal({ x: 10, y: 15 }).x).toBeCloseTo(0);
+      expect(lcs.toLocal({ x: 10, y: 15 }).y).toBeCloseTo(0);
+      expect(lcs.toLocal({ x: 11, y: 16 }).x).toBeCloseTo(1);
+      expect(lcs.toLocal({ x: 11, y: 16 }).y).toBeCloseTo(1);
+    });
+
+    test('toGlobal', () => {
+      const lcs = new LocalCoordinateSystem({ x: 10, y: 15, w: 20, h: 30, r: 0 });
+      expect(lcs.toGlobal({ x: 0, y: 0 }).x).toBeCloseTo(10);
+      expect(lcs.toGlobal({ x: 0, y: 0 }).y).toBeCloseTo(15);
+      expect(lcs.toGlobal({ x: 1, y: 1 }).x).toBeCloseTo(11);
+      expect(lcs.toGlobal({ x: 1, y: 1 }).y).toBeCloseTo(16);
+    });
+  });
+});
--- src/geometry/lcs.ts
+++ src/geometry/lcs.ts
@@ -1,41 +1,44 @@
-import { NOT_IMPLEMENTED_YET } from '../utils/assert.ts';
 import { Box } from './box.ts';
-import { Rotation } from './transform.ts';
+import { Rotation, Transform, Translation } from './transform.ts';
 import { Point } from './point.ts';
-import { Extent } from './extent.ts';
 
+// Note, this provides an "unscaled" local coordinate system.
 export class LocalCoordinateSystem {
-  static UNITY = new LocalCoordinateSystem({ x: 0, y: 0 }, { w: 1, h: 1 }, 0);
+  static UNITY = new LocalCoordinateSystem({ x: 0, y: 0, w: 1, h: 1, r: 0 });
 
   static fromBox(box: Box) {
     if (Point.isEqual(Point.ORIGIN, box) && box.r === 0 && box.w === 1 && box.h === 1)
       return this.UNITY;
-    return new LocalCoordinateSystem(box, box, box.r);
+    return new LocalCoordinateSystem(box);
   }
 
-  constructor(
-    private readonly origin: Point,
-    private readonly extent: Extent,
-    private readonly rotation: number
-  ) {}
+  constructor(private readonly box: Box) {}
 
   toGlobal(c: Box): Box;
   toGlobal(c: Point): Point;
   toGlobal(c: Box | Point): Box | Point {
-    // TODO: Need to use translation when needed
-    if (this.origin !== this.origin) NOT_IMPLEMENTED_YET();
-    if (this.extent !== this.extent) NOT_IMPLEMENTED_YET();
-    if ('pos' in c) return new Rotation(this.rotation).apply(c);
-    return new Rotation(this.rotation).apply(c);
+    if ('w' in c) {
+      return Transform.box(c as Box, new Rotation(this.box.r), new Translation(this.box));
+    } else {
+      return Transform.point(c as Point, new Rotation(this.box.r), new Translation(this.box));
+    }
   }
 
   toLocal(c: Box): Box;
   toLocal(c: Point): Point;
   toLocal(c: Box | Point): Box | Point {
-    // TODO: Need to use translation when needed
-    if (this.origin !== this.origin) NOT_IMPLEMENTED_YET();
-    if (this.extent !== this.extent) NOT_IMPLEMENTED_YET();
-    if ('pos' in c) return new Rotation(-this.rotation).apply(c);
-    return new Rotation(-this.rotation).apply(c);
+    if ('w' in c) {
+      return Transform.box(
+        c as Box,
+        new Translation({ x: -this.box.x, y: -this.box.y }),
+        new Rotation(-this.box.r)
+      );
+    } else {
+      return Transform.point(
+        c as Point,
+        new Translation({ x: -this.box.x, y: -this.box.y }),
+        new Rotation(-this.box.r)
+      );
+    }
   }
 }
