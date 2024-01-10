67c5f10b0b5a133b686802f08f9535e014f253ad
Magnus Johansson <magnus.johansson@se.ibm.com>
2024-01-10 21:12:46 +0100

    Cleanup: TransformPanel

--- src/react-app/ObjectProperties/TransformPanel.tsx
+++ src/react-app/ObjectProperties/TransformPanel.tsx
@@ -8,27 +8,25 @@ import { Point } from '../../geometry/point.ts';
 import { NumberInput } from '../NumberInput.tsx';
 import { useDiagram } from '../context/DiagramContext.tsx';
 import { ToolWindowPanel } from '../components/ToolWindowPanel.tsx';
+import { $c } from '../../utils/classname.ts';
 
 const origins: Record<string, Point> = {
-  'top-left': { x: 0, y: 0 },
-  'top-center': { x: 0.5, y: 0 },
-  'top-right': { x: 1, y: 0 },
-  'mid-left': { x: 0, y: 0.5 },
-  'mid-center': { x: 0.5, y: 0.5 },
-  'mid-right': { x: 1, y: 0.5 },
-  'bottom-left': { x: 0, y: 1 },
-  'bottom-center': { x: 0.5, y: 1 },
-  'bottom-right': { x: 1, y: 1 }
+  tl: { x: 0, y: 0 },
+  tc: { x: 0.5, y: 0 },
+  tr: { x: 1, y: 0 },
+  ml: { x: 0, y: 0.5 },
+  mc: { x: 0.5, y: 0.5 },
+  mr: { x: 1, y: 0.5 },
+  bl: { x: 0, y: 1 },
+  bc: { x: 0.5, y: 1 },
+  br: { x: 1, y: 1 }
 };
 
-// TODO: Implement transform origin
 export const TransformPanel = (props: Props) => {
   const diagram = useDiagram();
   const [bounds, setBounds] = useState<Box | undefined>(undefined);
   const [lockAspectRatio, setLockAspectRatio] = useState(false);
-  const [origin, setOrigin] = useState('top-left');
-
-  const aspectRatio = (bounds?.w ?? 1) / (bounds?.h ?? 1);
+  const [origin, setOrigin] = useState('tl');
 
   const transformedBounds = {
     x: (bounds?.x ?? 0) + (bounds?.w ?? 1) * origins[origin].x,
@@ -37,6 +35,8 @@ export const TransformPanel = (props: Props) => {
     h: bounds?.h ?? 1,
     r: bounds?.r ?? 0
   };
+
+  const aspectRatio = transformedBounds.w / transformedBounds.h;
 
   useEffect(() => {
     const callback = () => {
@@ -59,8 +59,8 @@ export const TransformPanel = (props: Props) => {
   //       ... especially all of the updating of the selection state
   const updateBounds = (bounds: Box) => {
     const newBounds = Box.asReadWrite(bounds);
-    newBounds.x -= (transformedBounds.w ?? 1) * origins[origin].x;
-    newBounds.y -= (transformedBounds.h ?? 1) * origins[origin].y;
+    newBounds.x -= transformedBounds.w * origins[origin].x;
+    newBounds.y -= transformedBounds.h * origins[origin].y;
 
     if (newBounds.w !== transformedBounds.w) {
       const dw = newBounds.w - transformedBounds.w;
@@ -73,28 +73,9 @@ export const TransformPanel = (props: Props) => {
     }
 
     if (newBounds.r !== transformedBounds.r) {
-      const newPos = Point.rotateAround(
-        {
-          x: 0,
-          y: 0
-        },
-        newBounds.r,
-        {
-          x: origins[origin].x - 0.5,
-          y: origins[origin].y - 0.5
-        }
-      );
-      const prevPos = Point.rotateAround(
-        {
-          x: 0,
-          y: 0
-        },
-        transformedBounds.r,
-        {
-          x: origins[origin].x - 0.5,
-          y: origins[origin].y - 0.5
-        }
-      );
+      const centerOfRotation = Point.subtract(origins[origin], { x: 0.5, y: 0.5 });
+      const newPos = Point.rotateAround(Point.ORIGIN, newBounds.r, centerOfRotation);
+      const prevPos = Point.rotateAround(Point.ORIGIN, transformedBounds.r, centerOfRotation);
       newBounds.x = newBounds.x - prevPos.x * transformedBounds.w + newPos.x * newBounds.w;
       newBounds.y = newBounds.y - prevPos.y * transformedBounds.h + newPos.y * newBounds.h;
     }
@@ -111,83 +92,45 @@ export const TransformPanel = (props: Props) => {
       title={'Transform'}
       hasCheckbox={false}
     >
-      <div className={'cmp-labeled-table'}>
-        <div
-          className={'cmp-labeled-table__label'}
-          style={{ alignSelf: 'start', marginTop: '0.1rem' }}
-        >
-          <svg width={'3rem'} height={'3rem'} viewBox={'0 0 1 1'}>
-            <rect
-              x={0.1}
-              y={0.1}
-              width={0.8}
-              height={0.8}
-              fill={'transparent'}
-              stroke={'var(--primary-fg)'}
-              strokeDasharray={'0.05 0.05'}
-              strokeWidth={0.02}
-            />
+      <div className={'cmp-labeled-table cmp-transform'}>
+        <div className={'cmp-labeled-table__label cmp-transform__graphics'}>
+          <svg viewBox={'0 0 1 1'}>
+            <rect className={'cmp-transform__rect'} x={0.1} y={0.1} width={0.8} height={0.8} />
             {Object.entries(origins).map(([k, v]) => (
               <circle
                 key={k}
+                className={$c('cmp-transform__node', { active: origin === k })}
                 cx={0.1 + v.x * 0.8}
                 cy={0.1 + v.y * 0.8}
                 r={0.08}
-                fill={origin === k ? 'var(--blue-11)' : 'var(--primary-bg)'}
-                stroke={origin === k ? 'var(--blue-11)' : 'var(--primary-fg)'}
-                strokeWidth={0.02}
-                cursor={'pointer'}
                 onClick={() => setOrigin(k)}
               />
             ))}
           </svg>
         </div>
         <div className={'cmp-labeled-table__value'}>
-          <div
-            style={{
-              display: 'grid',
-              gridTemplateAreas: '"x w" "y h" "r aspect-ratio"',
-              gridTemplateRows: 'repeat(3, 1fr)',
-              gridTemplateColumns: '1fr 1fr',
-              alignItems: 'center',
-              rowGap: '0.5rem',
-              columnGap: '0.3em'
-            }}
-          >
+          <div className={'cmp-transform__inputs'}>
             <div style={{ gridArea: 'x' }}>
               <NumberInput
-                style={{ width: '100%' }}
                 label={'x'}
                 value={round(transformedBounds.x)}
                 defaultUnit={'px'}
                 min={0}
-                onChange={ev => {
-                  updateBounds({
-                    ...transformedBounds,
-                    x: ev ?? 0
-                  });
-                }}
+                onChange={ev => updateBounds({ ...transformedBounds, x: ev ?? 0 })}
               />
             </div>
             <div style={{ gridArea: 'y' }}>
               <NumberInput
-                style={{ width: '100%' }}
                 label={'y'}
                 value={round(transformedBounds.y)}
                 defaultUnit={'px'}
                 min={0}
-                onChange={ev => {
-                  updateBounds({
-                    ...transformedBounds,
-                    y: ev ?? 0
-                  });
-                }}
+                onChange={ev => updateBounds({ ...transformedBounds, y: ev ?? 0 })}
               />
             </div>
             <div style={{ gridArea: 'w' }}>
               <NumberInput
-                style={{ width: '100%' }}
-                value={round(transformedBounds.w ?? 1)}
+                value={round(transformedBounds.w)}
                 label={'w'}
                 defaultUnit={'px'}
                 min={0}
@@ -202,8 +145,7 @@ export const TransformPanel = (props: Props) => {
             </div>
             <div style={{ gridArea: 'h' }}>
               <NumberInput
-                style={{ width: '100%' }}
-                value={round(transformedBounds.h ?? 1)}
+                value={round(transformedBounds.h)}
                 label={'h'}
                 defaultUnit={'px'}
                 min={0}
@@ -218,8 +160,7 @@ export const TransformPanel = (props: Props) => {
             </div>
             <div style={{ gridArea: 'r' }}>
               <NumberInput
-                style={{ width: '100%' }}
-                value={round(Angle.toDeg(transformedBounds.r ?? 0))}
+                value={round(Angle.toDeg(transformedBounds.r))}
                 label={'r'}
                 min={-360}
                 max={360}
@@ -228,7 +169,7 @@ export const TransformPanel = (props: Props) => {
                   const number = ev ?? 0;
                   updateBounds({
                     ...transformedBounds,
-                    r: Angle.toRad(isNaN(number) ? 0 : number ?? 0)
+                    r: Angle.toRad(isNaN(number) ? 0 : number)
                   });
                 }}
               />
