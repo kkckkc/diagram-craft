e3dc37de2de1d047bb0d8cc6def8704d09c5e3a8
Magnus Johansson <magnus.johansson@se.ibm.com>
2024-01-17 16:26:09 +0100

    Refactor: uow snapshot in shapeControlDrag.ts

--- src/base-ui/drag/shapeControlDrag.ts
+++ src/base-ui/drag/shapeControlDrag.ts
@@ -2,21 +2,34 @@ import { AbstractDrag, Modifiers } from './dragDropManager.ts';
 import { DiagramNode } from '../../model/diagramNode.ts';
 import { Point } from '../../geometry/point.ts';
 import { UnitOfWork } from '../../model/unitOfWork.ts';
+import { SnapshotUndoableAction } from '../../model/diagramUndoActions.ts';
 
 export class ShapeControlPointDrag extends AbstractDrag {
+  private readonly uow: UnitOfWork;
   constructor(
     private readonly node: DiagramNode,
     private readonly callback: (x: number, y: number, uow: UnitOfWork) => string
   ) {
     super();
+    this.uow = new UnitOfWork(this.node.diagram, true);
   }
 
   onDrag(coord: Point, _modifiers: Modifiers) {
-    UnitOfWork.execute(this.node.diagram, uow => {
-      const label = this.callback(coord.x, coord.y, uow);
-      this.setState({ label });
-    });
+    const label = this.callback(coord.x, coord.y, this.uow);
+    this.setState({ label });
+    this.uow.notify();
   }
 
-  onDragEnd(): void {}
+  onDragEnd(): void {
+    const snapshot = this.uow.commit();
+
+    this.node.diagram.undoManager.add(
+      new SnapshotUndoableAction(
+        'Adjust shape',
+        snapshot,
+        snapshot.retakeSnapshot(this.node.diagram),
+        this.node.diagram
+      )
+    );
+  }
 }
