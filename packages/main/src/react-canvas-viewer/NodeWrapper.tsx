4ab7bda911b9963e6c5f5afaa18eeb519aa37642
Magnus Johansson <magnus.johansson@se.ibm.com>
2024-03-31 19:55:10 +0200

    Optimize NodeWrapper

--- packages/main/src/react-canvas-viewer/NodeWrapper.tsx
+++ packages/main/src/react-canvas-viewer/NodeWrapper.tsx
@@ -23,35 +23,34 @@ export const NodeWrapper = (
   }
 
   return (
-    <>
-      <g style={{ filter: props.style?.filter }}>
-        <linearGradient
-          id={`reflection-grad-${props.node.id}`}
-          y2="1"
-          x2="0"
-          gradientUnits="objectBoundingBox"
-          gradientTransform={`rotate(${-Angle.toDeg(props.node.bounds.r)} 0.5 0.5)`}
-        >
-          <stop offset="0.65" stopColor="white" stopOpacity="0" />
-          <stop
-            offset="1"
-            stopColor="white"
-            stopOpacity={props.node.props.effects?.reflectionStrength}
-          />
-        </linearGradient>
+    <g style={{ filter: props.style?.filter }}>
+      {props.node.props.effects?.reflection && (
+        <>
+          <linearGradient
+            id={`reflection-grad-${props.node.id}`}
+            y2="1"
+            x2="0"
+            gradientUnits="objectBoundingBox"
+            gradientTransform={`rotate(${-Angle.toDeg(props.node.bounds.r)} 0.5 0.5)`}
+          >
+            <stop offset="0.65" stopColor="white" stopOpacity="0" />
+            <stop
+              offset="1"
+              stopColor="white"
+              stopOpacity={props.node.props.effects?.reflectionStrength}
+            />
+          </linearGradient>
 
-        <mask id={`reflection-mask-${props.node.id}`} maskContentUnits="objectBoundingBox">
-          <rect width="1" height="1" fill={`url(#reflection-grad-${props.node.id})`} />
-        </mask>
-
-        {props.node.props.effects?.reflection && (
+          <mask id={`reflection-mask-${props.node.id}`} maskContentUnits="objectBoundingBox">
+            <rect width="1" height="1" fill={`url(#reflection-grad-${props.node.id})`} />
+          </mask>
           <g
             transform={`
-            rotate(${-Angle.toDeg(props.node.bounds.r)} ${center.x} ${center.y})
-            scale(1 -1)
-            translate(0 -${2 * (pathBounds!.y + pathBounds!.h)})
-            rotate(${Angle.toDeg(props.node.bounds.r)} ${center.x} ${center.y})
-          `}
+                rotate(${-Angle.toDeg(props.node.bounds.r)} ${center.x} ${center.y})
+                scale(1 -1)
+                translate(0 -${2 * (pathBounds!.y + pathBounds!.h)})
+                rotate(${Angle.toDeg(props.node.bounds.r)} ${center.x} ${center.y})
+              `}
             mask={`url(#reflection-mask-${props.node.id})`}
             style={{
               filter: 'url(#reflection-filter)',
@@ -60,9 +59,9 @@ export const NodeWrapper = (
           >
             {props.children}
           </g>
-        )}
-        {props.children}
-      </g>
-    </>
+        </>
+      )}
+      {props.children}
+    </g>
   );
 };
