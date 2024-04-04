6a6d35e2f637183d945a4aa4d48880439b515fb8
Magnus Johansson <magnus.johansson@se.ibm.com>
2024-04-04 17:38:34 +0200

    React dep: migrate LabelNodeSelection

--- packages/main/src/react-canvas-editor/selection/LabelNodeSelection.tsx
+++ packages/main/src/react-canvas-editor/selection/LabelNodeSelection.tsx
@@ -2,13 +2,75 @@ import { DiagramNode } from '../../model/diagramNode.ts';
 import { assert, precondition } from '../../utils/assert.ts';
 import { Box } from '../../geometry/box.ts';
 import { TimeOffsetOnPath } from '../../geometry/pathPosition.ts';
-import { useDragDrop } from '../../react-canvas-viewer/DragDropManager.ts';
+import { DRAG_DROP_MANAGER } from '../../react-canvas-viewer/DragDropManager.ts';
 import { AttachmentPointDrag } from '../../base-ui/drag/attachmentPointDrag.ts';
+import { Component } from '../../base-ui/component.ts';
+import * as svg from '../../base-ui/vdom-svg.ts';
+import { useComponent } from '../../react-canvas-viewer/temp/useComponent.temp.ts';
+
+class LabelNodeSelectionComponent extends Component<Props> {
+  render(props: Props) {
+    precondition.is.present(props.node.props.labelForEdgeId);
+
+    const drag = DRAG_DROP_MANAGER;
+
+    const center = Box.center(props.node.bounds);
+    const edge = props.node.labelEdge();
+    assert.present(edge);
+
+    const labelNode = props.node.labelNode();
+    assert.present(labelNode);
+
+    const path = edge.path();
+    const attachmentPoint = path.pointAt(
+      TimeOffsetOnPath.toLengthOffsetOnPath({ pathT: labelNode.timeOffset! }, path)
+    );
+
+    return svg.g(
+      {},
+      svg.circle({
+        class: 'svg-selection__label-node',
+        cx: center.x,
+        cy: center.y,
+        r: 3
+      }),
+      svg.circle({
+        class: 'svg-selection__label-node',
+        cx: attachmentPoint.x,
+        cy: attachmentPoint.y,
+        r: 4,
+        on: {
+          mousedown: e => {
+            if (e.button !== 0) return;
+            drag.initiate(new AttachmentPointDrag(labelNode, edge, path));
+            e.stopPropagation();
+          }
+        }
+      }),
+      svg.line({
+        class: 'svg-selection__label-node__line',
+        x1: center.x,
+        y1: center.y,
+        x2: attachmentPoint.x,
+        y2: attachmentPoint.y
+      })
+    );
+  }
+}
 
 export const LabelNodeSelection = (props: Props) => {
+  const ref = useComponent<Props, LabelNodeSelectionComponent, SVGGElement>(
+    () => new LabelNodeSelectionComponent(),
+    props
+  );
+
+  return <g ref={ref}></g>;
+
+  /*
+
   precondition.is.present(props.node.props.labelForEdgeId);
 
-  const drag = useDragDrop();
+  const drag = DRAG_DROP_MANAGER;
 
   const center = Box.center(props.node.bounds);
   const edge = props.node.labelEdge();
@@ -45,6 +107,7 @@ export const LabelNodeSelection = (props: Props) => {
       />
     </>
   );
+   */
 };
 
 type Props = {
