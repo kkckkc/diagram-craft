441531c9efb8ffef88f43591237970eee4f6e4c2
Magnus Johansson <magnus.johansson@se.ibm.com>
2024-05-08 16:51:36 +0200

    DocumentTabs: improve overflow

--- packages/main/src/react-app/DocumentTabs.tsx
+++ packages/main/src/react-app/DocumentTabs.tsx
@@ -20,56 +20,58 @@ export const DocumentTabs = (props: Props) => {
   )?.id;
 
   return (
-    <Tabs.Root value={selection} onValueChange={props.onValueChange}>
-      <Tabs.List className="cmp-document-tabs" aria-label="Diagrams in document">
-        {props.document.diagrams.map(d => (
-          <Tabs.Trigger
-            key={d.id}
-            className="cmp-document-tabs__tab-trigger util-vcenter"
-            value={d.id}
-          >
-            <DocumentsContextMenu documentId={d.id}>
-              <div>
-                {d.name}
+    <div className={'cmp-document-tabs'}>
+      <Tabs.Root value={selection} onValueChange={props.onValueChange}>
+        <Tabs.List className="cmp-document-tabs__tabs" aria-label="Diagrams in document">
+          {props.document.diagrams.map(d => (
+            <Tabs.Trigger
+              key={d.id}
+              className="cmp-document-tabs__tab-trigger util-vcenter"
+              value={d.id}
+            >
+              <DocumentsContextMenu documentId={d.id}>
+                <div>
+                  {d.name}
 
-                {selection === d.id && selection !== props.value && (
-                  <span>&nbsp;&gt;&nbsp;{props.document.getById(props.value)!.name}</span>
-                )}
+                  {selection === d.id && selection !== props.value && (
+                    <span>&nbsp;&gt;&nbsp;{props.document.getById(props.value)!.name}</span>
+                  )}
 
-                {d.diagrams.length > 0 && (
-                  <div style={{ marginLeft: '0.35rem', marginTop: '0.1rem' }}>
-                    <TbFiles />
-                  </div>
-                )}
-              </div>
-            </DocumentsContextMenu>
-          </Tabs.Trigger>
-        ))}
-        <button
-          className={'cmp-document-tabs__add'}
-          onClick={() => {
-            const id = newid();
+                  {d.diagrams.length > 0 && (
+                    <div style={{ marginLeft: '0.35rem', marginTop: '0.1rem' }}>
+                      <TbFiles />
+                    </div>
+                  )}
+                </div>
+              </DocumentsContextMenu>
+            </Tabs.Trigger>
+          ))}
+        </Tabs.List>
+      </Tabs.Root>
+      <button
+        className={'cmp-document-tabs__add'}
+        onClick={() => {
+          const id = newid();
 
-            // TODO: Add undo here
+          // TODO: Add undo here
 
-            const diagram = new Diagram(
-              id,
-              'Sheet ' + (props.document.diagrams.length + 1).toString(),
-              props.document
-            );
-            diagram.layers.add(
-              new Layer('default', 'Default', [], diagram),
-              UnitOfWork.throwaway(diagram)
-            );
+          const diagram = new Diagram(
+            id,
+            'Sheet ' + (props.document.diagrams.length + 1).toString(),
+            props.document
+          );
+          diagram.layers.add(
+            new Layer('default', 'Default', [], diagram),
+            UnitOfWork.throwaway(diagram)
+          );
 
-            props.document.addDiagram(diagram);
-            props.onValueChange(id);
-          }}
-        >
-          <TbPlus />
-        </button>
-      </Tabs.List>
-    </Tabs.Root>
+          props.document.addDiagram(diagram);
+          props.onValueChange(id);
+        }}
+      >
+        <TbPlus />
+      </button>
+    </div>
   );
 };
 
