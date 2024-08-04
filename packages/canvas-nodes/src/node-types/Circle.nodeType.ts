7bf853ad71da5c4669d2ce9979e846c95d72f160
Magnus Johansson <magnus.johansson@se.ibm.com>
2024-08-04 21:12:28 +0200

    Fix: orientation of circle

--- packages/canvas-nodes/src/node-types/Circle.nodeType.ts
+++ packages/canvas-nodes/src/node-types/Circle.nodeType.ts
@@ -4,8 +4,8 @@ import {
   BaseShapeBuildShapeProps
 } from '@diagram-craft/canvas/components/BaseNodeComponent';
 import { ShapeBuilder } from '@diagram-craft/canvas/shape/ShapeBuilder';
-import { PathBuilder, unitCoordinateSystem } from '@diagram-craft/geometry/pathBuilder';
-import { Point } from '@diagram-craft/geometry/point';
+import { PathBuilder, simpleCoordinateSystem } from '@diagram-craft/geometry/pathBuilder';
+import { _p } from '@diagram-craft/geometry/point';
 import { DiagramNode } from '@diagram-craft/model/diagramNode';
 import { Anchor } from '@diagram-craft/model/anchor';
 
@@ -16,21 +16,21 @@ export class CircleNodeDefinition extends ShapeNodeDefinition {
 
   getShapeAnchors(_def: DiagramNode): Anchor[] {
     return [
-      { start: Point.of(0.5, 0), id: '1', type: 'point', isPrimary: true, normal: -Math.PI / 2 },
-      { start: Point.of(1, 0.5), id: '2', type: 'point', isPrimary: true, normal: 0 },
-      { start: Point.of(0.5, 1), id: '3', type: 'point', isPrimary: true, normal: Math.PI / 2 },
-      { start: Point.of(0, 0.5), id: '4', type: 'point', isPrimary: true, normal: Math.PI },
-      { start: Point.of(0.5, 0.5), clip: true, id: 'c', type: 'center' }
+      { start: _p(0.5, 0), id: '1', type: 'point', isPrimary: true, normal: -Math.PI / 2 },
+      { start: _p(1, 0.5), id: '2', type: 'point', isPrimary: true, normal: 0 },
+      { start: _p(0.5, 1), id: '3', type: 'point', isPrimary: true, normal: Math.PI / 2 },
+      { start: _p(0, 0.5), id: '4', type: 'point', isPrimary: true, normal: Math.PI },
+      { start: _p(0.5, 0.5), clip: true, id: 'c', type: 'center' }
     ];
   }
 
   getBoundingPathBuilder(def: DiagramNode) {
-    const b = new PathBuilder(unitCoordinateSystem(def.bounds));
-    b.moveTo(Point.of(0, -1));
-    b.arcTo(Point.of(1, 0), 1, 1);
-    b.arcTo(Point.of(0, 1), 1, 1);
-    b.arcTo(Point.of(-1, 0), 1, 1);
-    b.arcTo(Point.of(0, -1), 1, 1);
+    const b = new PathBuilder(simpleCoordinateSystem(def.bounds));
+    b.moveTo(_p(0.5, 0));
+    b.arcTo(_p(1, 0.5), 0.5, 0.5, 0, 0, 1);
+    b.arcTo(_p(0.5, 1), 0.5, 0.5, 0, 0, 1);
+    b.arcTo(_p(0, 0.5), 0.5, 0.5, 0, 0, 1);
+    b.arcTo(_p(0.5, 0), 0.5, 0.5, 0, 0, 1);
     return b;
   }
 }
