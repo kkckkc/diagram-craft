0d58d174bd141522854800623fae7b403c6f9722
Magnus Johansson <magnus.johansson@se.ibm.com>
2024-08-05 20:52:53 +0200

    Fix: orientation of parallelogram

--- packages/canvas-nodes/src/node-types/Parallelogram.nodeType.ts
+++ packages/canvas-nodes/src/node-types/Parallelogram.nodeType.ts
@@ -4,8 +4,8 @@ import {
   BaseShapeBuildShapeProps
 } from '@diagram-craft/canvas/components/BaseNodeComponent';
 import { ShapeBuilder } from '@diagram-craft/canvas/shape/ShapeBuilder';
-import { PathBuilder, unitCoordinateSystem } from '@diagram-craft/geometry/pathBuilder';
-import { Point } from '@diagram-craft/geometry/point';
+import { PathBuilder, simpleCoordinateSystem } from '@diagram-craft/geometry/pathBuilder';
+import { _p } from '@diagram-craft/geometry/point';
 import { DiagramNode } from '@diagram-craft/model/diagramNode';
 import { CustomPropertyDefinition } from '@diagram-craft/model/elementDefinitionRegistry';
 import { UnitOfWork } from '@diagram-craft/model/unitOfWork';
@@ -26,54 +26,48 @@ export class ParallelogramNodeDefinition extends ShapeNodeDefinition {
     super('parallelogram', 'Parallelogram', ParallelogramComponent);
   }
 
-  getCustomPropertyDefinitions(def: DiagramNode): Array<CustomPropertyDefinition> {
+  getCustomPropertyDefinitions(node: DiagramNode): Array<CustomPropertyDefinition> {
     return [
       {
         id: 'slant',
         type: 'number',
         label: 'Slant',
-        value: def.renderProps.custom.parallelogram.slant,
+        value: node.renderProps.custom.parallelogram.slant,
         maxValue: 60,
         unit: 'px',
         onChange: (value: number, uow: UnitOfWork) => {
-          if (value >= def.bounds.w / 2 || value >= def.bounds.h / 2) return;
-          def.updateCustomProps('parallelogram', props => (props.slant = value), uow);
+          if (value >= node.bounds.w / 2 || value >= node.bounds.h / 2) return;
+          node.updateCustomProps('parallelogram', props => (props.slant = value), uow);
         }
       }
     ];
   }
 
-  getBoundingPathBuilder(def: DiagramNode) {
-    const slant = def.renderProps.custom.parallelogram.slant;
-    const bnd = def.bounds;
+  getBoundingPathBuilder(node: DiagramNode) {
+    const slant = node.renderProps.custom.parallelogram.slant;
+    const slantPct = slant / node.bounds.w;
 
-    const sr = slant / bnd.w;
-    const cds = sr * 2;
-
-    const pathBuilder = new PathBuilder(unitCoordinateSystem(def.bounds));
-
-    pathBuilder.moveTo(Point.of(-1 + cds, 1));
-    pathBuilder.lineTo(Point.of(1, 1));
-    pathBuilder.lineTo(Point.of(1 - cds, -1));
-    pathBuilder.lineTo(Point.of(-1, -1));
-    pathBuilder.lineTo(Point.of(-1 + cds, 1));
-
-    return pathBuilder;
+    return new PathBuilder(simpleCoordinateSystem(node.bounds))
+      .moveTo(_p(slantPct, 0))
+      .lineTo(_p(1, 0))
+      .lineTo(_p(1 - slantPct, 1))
+      .lineTo(_p(0, 1))
+      .lineTo(_p(slantPct, 0));
   }
 }
 
 class ParallelogramComponent extends BaseNodeComponent {
   buildShape(props: BaseShapeBuildShapeProps, shapeBuilder: ShapeBuilder) {
     const slant = props.nodeProps.custom.parallelogram.slant;
-    const boundary = new ParallelogramNodeDefinition()
-      .getBoundingPathBuilder(props.node)
-      .getPaths();
 
-    shapeBuilder.boundaryPath(boundary.all());
+    shapeBuilder.boundaryPath(
+      new ParallelogramNodeDefinition().getBoundingPathBuilder(props.node).getPaths().all()
+    );
+
     shapeBuilder.text(this);
 
     shapeBuilder.controlPoint(
-      Point.of(props.node.bounds.x + slant, props.node.bounds.y),
+      _p(props.node.bounds.x + slant, props.node.bounds.y),
       ({ x }, uow) => {
         const distance = Math.max(0, x - props.node.bounds.x);
         if (distance < props.node.bounds.w / 2 && distance < props.node.bounds.h / 2) {
