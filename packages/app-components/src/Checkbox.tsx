3c43bca6e238060faa0644b9fb5063219062c8ec
Magnus Johansson <magnus.johansson@se.ibm.com>
2024-09-10 21:07:07 +0200

    Feat: add FancyCheckbox component

--- packages/app-components/src/Checkbox.tsx
+++ packages/app-components/src/Checkbox.tsx
@@ -1,17 +1,43 @@
 import { extractMouseEvents } from './utils';
+import { ToggleButtonGroup } from './ToggleButtonGroup';
+import { TbCheckbox, TbSquare } from 'react-icons/tb';
 
 export const Checkbox = (props: Props) => {
-  // TODO: Implement interminate state
+  // TODO: Implement indeterminate state
   return (
-    <input
-      {...extractMouseEvents(props)}
-      type="checkbox"
-      checked={props.value}
-      data-field-state={props.isIndeterminate ? 'indeterminate' : props.state}
-      onChange={e => {
-        props.onChange(e.target.checked);
-      }}
-    />
+    <>
+      <input
+        {...extractMouseEvents(props)}
+        type="checkbox"
+        checked={props.value}
+        data-field-state={props.isIndeterminate ? 'indeterminate' : props.state}
+        onChange={e => {
+          props.onChange(e.target.checked);
+        }}
+      />
+      {props.label && <span>&nbsp;{props.label}</span>}
+    </>
+  );
+};
+
+export const FancyCheckbox = (props: Props) => {
+  return (
+    <>
+      <ToggleButtonGroup.Root
+        type={'single'}
+        value={props.value ? 'set' : ''}
+        data-field-state={props.isIndeterminate ? 'indeterminate' : props.state}
+        onChange={value => {
+          props.onChange(value === 'set');
+        }}
+      >
+        <ToggleButtonGroup.Item value={'set'}>
+          <div style={{ display: 'flex', gap: '0.5rem' }}>
+            {props.value ? <TbCheckbox /> : <TbSquare />} {props.label}
+          </div>
+        </ToggleButtonGroup.Item>
+      </ToggleButtonGroup.Root>
+    </>
   );
 };
 
@@ -20,4 +46,5 @@ type Props = {
   state?: 'set' | 'unset' | 'overridden';
   isIndeterminate?: boolean;
   onChange: (value: boolean | undefined) => void;
+  label?: string;
 };
