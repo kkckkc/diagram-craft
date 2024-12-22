322f8391b0bf87d75e38d93b2bf57f10c563ba80
Magnus Johansson <magnus.johansson@se.ibm.com>
2024-12-22 20:02:25 +0100

    Feat: ability to edit indicators

--- packages/main/src/react-app/RightSidebar.tsx
+++ packages/main/src/react-app/RightSidebar.tsx
@@ -1,8 +1,9 @@
 import { SideBar, SideBarPage } from './SideBar';
-import { TbDatabaseEdit, TbInfoCircle, TbPalette } from 'react-icons/tb';
+import { TbBadge, TbDatabaseEdit, TbInfoCircle, TbPalette } from 'react-icons/tb';
 import { ObjectToolWindow } from './toolwindow/ObjectToolWindow/ObjectToolWindow';
 import { ObjectInfoToolWindow } from './toolwindow/ObjectInfoToolWindow/ObjectInfoToolWindow';
 import { ObjectDataToolWindow } from './toolwindow/ObjectDataToolWindow/ObjectDataToolWindow';
+import { ObjectIndicatorToolWindow } from './toolwindow/ObjectIndicatorToolWindow/ObjectIndicatorToolWindow';
 
 export const RightSidebar = () => {
   return (
@@ -15,6 +16,9 @@ export const RightSidebar = () => {
       </SideBarPage>
       <SideBarPage icon={TbDatabaseEdit}>
         <ObjectDataToolWindow />
+      </SideBarPage>
+      <SideBarPage icon={TbBadge}>
+        <ObjectIndicatorToolWindow />
       </SideBarPage>
     </SideBar>
   );
